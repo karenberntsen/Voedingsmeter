09b62c5088848521d7b59be50c188c77bbd5046a
Karen <berntsenkaren@hotmail.com>
2017-10-11 13:19:29 +0200

    test

--- src/main/java/nl/voeding/voedingsmeter/rest/VoedingsmeterEndpoint.java
+++ src/main/java/nl/voeding/voedingsmeter/rest/VoedingsmeterEndpoint.java
@@ -1,5 +1,7 @@
 package nl.voeding.voedingsmeter.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +16,21 @@ public class VoedingsmeterEndpoint {
 	@Autowired
 	VoedingsmeterService voedingsmeterService;
 	
-	@GetMapping("/product")
+	@GetMapping("/hallo")
 	public String getHallo() {
 		return "Hallo allemaal";
 	}
 	
-	@GetMapping("/product2")
-	public Product getProduct() {
+	@GetMapping("/createProduct")
+	public Product createProduct() {
 		Product appel = new Product("appel",100f,Eenheid.GRAM,60f,0f,0.2f,0f,0.2f,13f,10.4f,null,2.0f,0.003f,"http://www.voedingscentrum.nl/encyclopedie/appel.aspx");
 		voedingsmeterService.save(appel);
 		return appel;
 	}
-	
+
+	@GetMapping("/getProducts")
+	public List<Product> getProducts() {
+		return voedingsmeterService.getAll();
+	}
+
 }
